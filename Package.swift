a492a55b635000e194d5cdf3d907284f36d830fc
burkaydurdu <burkaydurdu@outlook.com>
2021-06-09 17:30:15 +0300

    Add xcframwork target

--- Package.swift
+++ Package.swift
@@ -18,10 +18,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Kezban",
-            dependencies: [],
-            path: "Kezban/"),
+        /* .target( */
+        /*     name: "Kezban", */
+        /*     dependencies: [], */
+        /*     path: "Kezban/"), */
+        .binaryTarget(
+          name: "Kezban",
+          path: "Kezban.xcframework"),
         .testTarget(
             name: "KezbanTests",
             dependencies: ["Kezban"]),
